954bd634488a6b853107125e7fe8a933d7bd474c
andrewShillito <34523587+A-S-8@users.noreply.github.com>
2019-01-16 11:29:56 -0500

    feat: dummy deck data

---
+++ utils/_data.js
@@ -0,0 +1,24 @@
+const data = {
+  React: {
+    title: 'React',
+    questions: [
+      {
+        question: 'What is React?',
+        answer: 'A library for managing user interfaces'
+      },
+      {
+        question: 'Where do you make Ajax requests in React?',
+        answer: 'The componentDidMount lifecycle event'
+      }
+    ]
+  },
+  JavaScript: {
+    title: 'JavaScript',
+    questions: [
+      {
+        question: 'What is a closure?',
+        answer: 'The combination of a function and the lexical environment within which that function was declared.'
+      }
+    ]
+  }
+}
