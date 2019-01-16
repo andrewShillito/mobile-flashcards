f08f45d744faed85335c3304c145c008cc846ce6
andrewShillito <34523587+A-S-8@users.noreply.github.com>
2019-01-16 12:14:43 -0500

    feat: build initial api mimicking functions

--- utils/_data.js
+++ utils/_data.js
@@ -1,4 +1,4 @@
-const data = {
+let decks = {
   React: {
     title: 'React',
     questions: [
@@ -22,3 +22,56 @@ const data = {
     ]
   }
 }
+
+const formatDeck = function(title) {
+  return {
+    [title]: {
+      title,
+      questions: [],
+    },
+  };
+}
+
+const _getDecks = function() {
+  return new Promise((res, rej) => {
+    setTimeout(() => res({...decks}), 1000);
+  });
+}
+
+const _getDeck = function(id) {
+  return new Promise((res, rej) => {
+    setTimeout(() => {
+      decks[id] !== undefined
+        ? res({...decks[id]})
+        : rej(Error("Deck not found"));
+    }, 500);
+  });
+}
+
+const _saveDeckTitle = function(title) {
+  return new Promise((res, rej) => {
+    setTimeout(() => {
+      decks[title] !== undefined
+        ? rej(Error("That deck name is already in use"))
+        : decks[title] = formatDeck(title), res(decks[title]);
+    }, 500);
+  });
+}
+
+const _addCardToDeck(title, card) {
+  return new Promise((res, rej) => {
+    decks[title] !== undefined
+      ? decks[title] = {
+          ...decks[title],
+          "questions": decks[title].questions.concat([card]),
+        }
+      : rej(Error("Deck not found"));
+  });
+}
+
+export {
+  _getDecks,
+  _getDeck,
+  _saveDeckTitle,
+  _addCardToDeck,
+};
