135f4e2c30cd36461849da8aebb3eb1f1b2c1277
andrewShillito <34523587+A-S-8@users.noreply.github.com>
2019-01-21 12:01:47 -0500

    feat: begin building out AddCard ui start changing SubmitBtn to accept disabled prop & type prop for styling

---
+++ components/AddCard.js
@@ -0,0 +1,52 @@
+import React from "react";
+import { View, Text, TextInput } from "react-native";
+import { connect } from "react-redux";
+import RouteTitle from "./RouteTitle";
+import SubmitBtn from "./SubmitBtn";
+import { inputStyles } from "../styles";
+
+class AddCard extends React.Component {
+  state = {
+    question: "",
+    answer: "",
+  }
+  onChange = ({value, name}) => {
+    this.setState(() => ({
+      [name]: value,
+    }));
+  }
+  validateInput = () => {
+    return (this.state.question.length && this.state.answer.length);
+  }
+  render() {
+    const title = this.props.navigation.state.params.title;
+    const validInput = this.validateInput;
+
+    return (
+      <View style={{flex:1}}>
+        <RouteTitle>{`Add card to ${title}`}</RouteTitle>
+        <View style={inputStyles.inputContainer}>
+          <TextInput
+            style={inputStyles.input}
+            placeholder="Question"
+            onChangeText={(question) => this.onChange({ value: question, name: "question" })}
+            value={this.state.question}
+            />
+        </View>
+        <View style={inputStyles.inputContainer}>
+          <TextInput
+            style={inputStyles.input}
+            placeholder="Answer"
+            onChangeText={(answer) => this.onChange({ value: answer, name: "answer" })}
+            value={this.state.answer}
+            />
+        </View>
+        <SubmitBtn disabled={!validInput} onPress={}>Submit</SubmitBtn>
+      </View>
+    );
+  }
+}
+
+
+
+export default connect()(AddCard);
---
+++ components/SubmitBtn.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { View, Text, TouchableOpacity } from "react-native";
+import { buttonStyles as styles } from "../styles";
+
+function SubmitBtn({ onPress, children, disabled }) {
+  return (
+    <TouchableOpacity style={styles.submitBtn} onPress={onPress}>
+      <Text style={styles.text}>{children}</Text>
+    </TouchableOpacity>
+  )
+}
+
+export default SubmitBtn;
