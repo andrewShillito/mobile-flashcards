a229c22c32d5844ee1856a1a1f6c709fd72eafe6
andrewShillito <34523587+A-S-8@users.noreply.github.com>
2019-01-21 12:21:32 -0500

    refactor: remove unnecesary code in SubmitBtn

--- components/SubmitBtn.js
+++ components/SubmitBtn.js
@@ -2,20 +2,7 @@ import React from "react";
 import { View, Text, TouchableOpacity } from "react-native";
 import { buttonStyles as styles } from "../styles";
 
-function SubmitBtn({ onPress, children, disabled, type }) {
-  if (disabled !== undefined){
-    disabled
-    ? return (
-        <TouchableOpacity style={styles[type]} onPress={onPress}>
-          <Text style={styles.text}>{children}</Text>
-        </TouchableOpacity>
-      );
-    : return (
-        <TouchableOpacity style={styles[type]} onPress={onPress}>
-          <Text style={styles.text}>{children}</Text>
-        </TouchableOpacity>
-      );
-  }
+function SubmitBtn({ onPress, children, type }) {
   return (
      <TouchableOpacity style={styles[type]} onPress={onPress}>
        <Text style={styles.text}>{children}</Text>
