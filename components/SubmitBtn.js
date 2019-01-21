d1f1d112feb8d02c1a71002a4535e8736912322b
andrewShillito <34523587+A-S-8@users.noreply.github.com>
2019-01-21 12:19:23 -0500

    feat: adding style flexibility and setting up for future disabled styling

--- components/SubmitBtn.js
+++ components/SubmitBtn.js
@@ -2,12 +2,25 @@ import React from "react";
 import { View, Text, TouchableOpacity } from "react-native";
 import { buttonStyles as styles } from "../styles";
 
-function SubmitBtn({ onPress, children, disabled }) {
+function SubmitBtn({ onPress, children, disabled, type }) {
+  if (disabled !== undefined){
+    disabled
+    ? return (
+        <TouchableOpacity style={styles[type]} onPress={onPress}>
+          <Text style={styles.text}>{children}</Text>
+        </TouchableOpacity>
+      );
+    : return (
+        <TouchableOpacity style={styles[type]} onPress={onPress}>
+          <Text style={styles.text}>{children}</Text>
+        </TouchableOpacity>
+      );
+  }
   return (
-    <TouchableOpacity style={styles.submitBtn} onPress={onPress}>
-      <Text style={styles.text}>{children}</Text>
-    </TouchableOpacity>
-  )
+     <TouchableOpacity style={styles[type]} onPress={onPress}>
+       <Text style={styles.text}>{children}</Text>
+     </TouchableOpacity>
+   );
 }
 
 export default SubmitBtn;
