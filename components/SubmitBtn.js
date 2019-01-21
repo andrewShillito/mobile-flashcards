94e2369ad9bb2dbd60381e41c84386cc8baac989
andrewShillito <34523587+A-S-8@users.noreply.github.com>
2019-01-21 12:22:44 -0500

    fix: submitBtn default styling

--- components/SubmitBtn.js
+++ components/SubmitBtn.js
@@ -3,6 +3,9 @@ import { View, Text, TouchableOpacity } from "react-native";
 import { buttonStyles as styles } from "../styles";
 
 function SubmitBtn({ onPress, children, type }) {
+  if (type === undefined) {
+    type = "submitBtn";
+  }
   return (
      <TouchableOpacity style={styles[type]} onPress={onPress}>
        <Text style={styles.text}>{children}</Text>
