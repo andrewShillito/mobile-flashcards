4c90cb9764c60383d045cc0e76404fa2ba7895da
andrewShillito <34523587+A-S-8@users.noreply.github.com>
2019-01-24 14:05:00 -0500

    feat: more quiz functionality and styling

--- components/Quiz.js
+++ components/Quiz.js
@@ -1,14 +1,17 @@
 import React from "react";
-import { View, Text } from "react-native";
+import { View, Text, TouchableOpacity } from "react-native";
 import { connect } from "react-redux";
 import Question from "./Question";
-import SubmitBtn from "./SubmitBtn"
+import SubmitBtn from "./SubmitBtn";
+import { FontAwesome } from "@expo/vector-icons"
 
 class Quiz extends React.Component {
   state = {
     index: 0,
     showAnswer: false,
     message: "Show Answer",
+    numCorrect: 0,
+    scoreData: {},
   }
   increment = () => {
     this.setState((prevState) => ({
@@ -32,9 +35,35 @@ class Quiz extends React.Component {
       message: prevState.message === "Show Answer" ? "Hide Answer" : "Show Answer",
     }));
   }
+  markCorrect = () => {
+    this.setState((prevState) => {
+      let scoreData = { ...prevState.scoreData, [prevState.index]: true };
+      return {
+        numCorrect: this.getNumCorrect(scoreData),
+        scoreData,
+      };
+    })
+  }
+  markIncorrect = () => {
+    this.setState((prevState) => {
+      let scoreData = { ...prevState.scoreData, [prevState.index]: false }
+      return {
+        numCorrect: this.getNumCorrect(scoreData),
+        scoreData,
+      };
+    })
+  }
+  getNumCorrect = (scoreData) => {
+    return Object.keys(scoreData).reduce((acc, curr) => {
+      if (scoreData[curr] === true) {
+        return acc + 1;
+      }
+      return acc;
+    }, 0)
+  }
   render() {
     const deck = this.props.deck;
-
+    console.log("Quiz State:", this.state);
     if (this.state.index < deck.questions.length) {
       const card = deck.questions[this.state.index];
       const { question, answer } = card;
@@ -43,6 +72,16 @@ class Quiz extends React.Component {
           <Text style={{alignSelf: "center", justifyContent: "flex-start", fontSize: 20}}>{`${this.state.index + 1} of ${deck.questions.length}`}</Text>
           <Question question={question} answer={answer} showAnswer={this.state.showAnswer} />
           <SubmitBtn onPress={this.toggleShowAnswer} type="textButton">{this.state.message}</SubmitBtn>
+          <View style={{flexDirection: "row", justifyContent: "space-around"}}>
+            <TouchableOpacity onPress={this.markIncorrect} style={{justifyContent: "center", alignItems: "center"}}>
+              <Text style={{fontSize: 20, marginBottom: 5}}>Incorrect</Text>
+              <FontAwesome name="window-close" size={50} color="red" />
+            </TouchableOpacity>
+            <TouchableOpacity onPress={this.markCorrect} style={{justifyContent: "center", alignItems: "center"}}>
+              <Text style={{fontSize: 20, marginBottom: 5}}>Correct</Text>
+              <FontAwesome name="check-square-o" size={50} color="green" />
+            </TouchableOpacity>
+          </View>
           <View style={{flexDirection: "row", justifyContent: "space-between", alignItems: "flex-end"}}>
             <SubmitBtn onPress={this.decrement} type="textButton">Previous Question</SubmitBtn>
             <SubmitBtn onPress={this.increment} type="textButton">Next Question</SubmitBtn>
