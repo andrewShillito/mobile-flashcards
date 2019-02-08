a31a670f3334a0bde7be3fc05bdddb387c3f6cef
andrewShillito <34523587+A-S-8@users.noreply.github.com>
2019-02-08 11:25:01 -0500

    feat: initialize SelectDeckModal and connect Edit Component

--- components/Edit.js
+++ components/Edit.js
@@ -1,14 +1,17 @@
 import React from "react";
-import { View, Text, Modal, TouchableHighlight } from "react-native";
+import { View, Text, TextInput, TouchableOpacity } from "react-native";
+import { connect } from "react-redux";
+import SelectDeckModal from "./SelectDeckModal";
 
 class Edit extends React.Component{
   render() {
     return (
-      <View>
+      <View style={{flex: 1, justifyContent: "space-evenly", alignItems: "center"}}>
         <Text>Edit Route</Text>
+
       </View>
     );
   }
 }
 
-export default Edit;
+export default connect()(Edit);
---
+++ components/SelectDeckModal.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { View, Text, TextInput, TouchableOpacity } from "react-native";
+
+class SelectDeckModal extends React.Component{
+  render() {
+    return (
+      <View>
+        <Text>Modal</Text>
+      </View>
+    );
+  }
+}
+
+export default SelectDeckModal;
