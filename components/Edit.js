26b783deb6fb3cf58e83271c0dab4220fa06d5d3
andrewShillito <34523587+A-S-8@users.noreply.github.com>
2019-02-08 13:05:06 -0500

    feat: edit tab can render modal - flatList not currently working

--- components/Edit.js
+++ components/Edit.js
@@ -2,13 +2,14 @@ import React from "react";
 import { View, Text, TextInput, TouchableOpacity } from "react-native";
 import { connect } from "react-redux";
 import SelectDeckModal from "./SelectDeckModal";
+import SubmitBtn from "./SubmitBtn";
 
 class Edit extends React.Component{
   render() {
     return (
       <View style={{flex: 1, justifyContent: "space-evenly", alignItems: "center"}}>
         <Text>Edit Route</Text>
-
+        <SelectDeckModal></SelectDeckModal>
       </View>
     );
   }
--- components/SelectDeckModal.js
+++ components/SelectDeckModal.js
@@ -1,14 +1,75 @@
 import React from "react";
-import { View, Text, TextInput, TouchableOpacity } from "react-native";
+import { View, Text, Modal, TouchableOpacity, FlatList } from "react-native";
+import SubmitBtn from "./SubmitBtn";
+import { buttonStyles } from "../styles";
+import { FontAwesome } from "@expo/vector-icons";
+import { connect } from "react-redux";
 
 class SelectDeckModal extends React.Component{
-  render() {
+  state = {
+    modalVisible: false,
+    selectedDeck: null,
+  }
+  setModalVisibility(visible) {
+    this.setState(() => ({
+      modalVisible: visible,
+    }));
+  }
+  renderItem({ item }) {
     return (
       <View>
-        <Text>Modal</Text>
+        <TouchableOpacity onPress={() => this.setDeck(item.title)}>
+          <Text style={{color: "white"}}>{item.title}</Text>
+          <Text>does this work?</Text>
+        </TouchableOpacity>
+      </View>
+    );
+  }
+  setDeck(title) {
+    this.setState(() => ({
+      selectedDeck: title,
+    }));
+    this.setModalVisibility(false);
+  }
+  render() {
+    const { decks } = this.props;
+
+    return (
+      <View>
+        <Modal
+          visible={this.state.modalVisible}
+          onRequestClose={() => alert("Modal closed")}
+          animationType="slide"
+          transparent={true}
+          >
+          <View style={{marginTop: 22, marginHorizontal: 22, backgroundColor: "gray", alignSelf: "center", width: 300, height: 300,}}>
+            <TouchableOpacity
+              onPress={() => this.setModalVisibility(false)}
+              style={{alignSelf: "flex-end", justifyContent: "flex-start"}}
+              >
+              <FontAwesome name="close" size={30} color="red" />
+            </TouchableOpacity>
+            <FlatList
+              data={decks}
+              renderItem={this.renderItem}
+              keyExtractor={(item) => item.title}
+              />
+          </View>
+        </Modal>
+        <TouchableOpacity style={buttonStyles.textButton} onPress={() => this.setModalVisibility(true)}>
+          <Text style={buttonStyles.textButtonText}>Show Modal</Text>
+        </TouchableOpacity>
       </View>
     );
   }
 }
 
-export default SelectDeckModal;
+function mapStateToProps({ decks }) {
+  return { //returning array
+    decks: Object.keys(decks).map(title => {
+      return decks[title];
+    })
+  };
+}
+
+export default connect(mapStateToProps)(SelectDeckModal);
