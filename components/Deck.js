c59d6bb1268bfd88b6544541224c67529741930f
andrewShillito <34523587+A-S-8@users.noreply.github.com>
2019-02-09 17:25:06 -0500

    feat: begin self-implemented deck swiping

--- components/Deck.js
+++ components/Deck.js
@@ -7,38 +7,54 @@ import { FontAwesome, MaterialCommunityIcons } from "@expo/vector-icons";
 class Deck extends React.Component {
   state = {
     marginRight: new Animated.Value(0),
-
+    pannedLeft: false,
   }
 
   handleTouch = () => {
+    let finalVal = 60
+    if (this.state.pannedLeft) {
+      finalVal = 0;
+    }
     Animated.timing(
       this.state.marginRight,
       {
-        toValue: 20,
-        duration: 1000,
+        toValue: finalVal,
+        duration: 300,
       }
-    ).start();
+    ).start(() => this.setState((prevState) => ({ pannedLeft: !prevState.pannedLeft })));
   }
   render() {
     const { deck, onPress } = this.props;
     const marginRight = this.state.marginRight;
+    const showButtons = this.state.pannedLeft;
 
     const rightButtons =
-      <View>
+      <Animated.View>
         <TouchableOpacity onPress={() => onPress(deck.title)}>
           <FontAwesome name="info" size={30} color="green"></FontAwesome>
-        </TouchableOpacity>,
+        </TouchableOpacity>
         <TouchableOpacity onPress={() => onPress(deck.title)}>
           <MaterialCommunityIcons name="cards" size={30} color="green"></MaterialCommunityIcons>
         </TouchableOpacity>
-      </View>
+      </Animated.View>
 
     return (
-      <Animated.View style={{marginRight: marginRight}}>
+      <Animated.View style={{marginRight: marginRight, flexDirection: "row"}}>
         <TouchableOpacity style={styles.deck} onPress={this.handleTouch}>
           <DeckHeader>{deck.title}</DeckHeader>
           <Text style={styles.text}>{`${deck.questions.length} card${deck.questions.length>1 || deck.questions.length === 0 ? "s" : ""}`}</Text>
         </TouchableOpacity>
+        {showButtons
+          ? <Animated.View style={{justifyContent: "space-evenly", alignItems: "center"}}>
+              <TouchableOpacity onPress={() => onPress(deck.title)}>
+                <FontAwesome name="info" size={40} color="green"></FontAwesome>
+              </TouchableOpacity>
+              <TouchableOpacity onPress={() => onPress(deck.title)}>
+                <MaterialCommunityIcons name="cards" size={40} color="green"></MaterialCommunityIcons>
+              </TouchableOpacity>
+            </Animated.View>
+          : null
+      }
       </Animated.View>
     );
   }
