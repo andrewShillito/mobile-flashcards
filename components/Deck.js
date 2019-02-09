5702fc58a55db12e5ea1a46f58af833b5d769fa3
andrewShillito <34523587+A-S-8@users.noreply.github.com>
2019-02-09 17:07:36 -0500

    feat: animation proof of concept

--- components/Deck.js
+++ components/Deck.js
@@ -1,34 +1,45 @@
 import React from 'react';
-import { Text, View, TouchableOpacity } from 'react-native';
+import { Text, View, TouchableOpacity, Animated } from 'react-native';
 import DeckHeader from "./DeckHeader";
 import { deckStyles as styles } from "../styles";
-import Swipeable from "react-native-swipeable";
 import { FontAwesome, MaterialCommunityIcons } from "@expo/vector-icons";
 
 class Deck extends React.Component {
-  swipeable = null;
-  handleUserBeganScrollingParentView() {
-    this.swipeable.recenter();
+  state = {
+    marginRight: new Animated.Value(0),
+
+  }
+
+  handleTouch = () => {
+    Animated.timing(
+      this.state.marginRight,
+      {
+        toValue: 20,
+        duration: 1000,
+      }
+    ).start();
   }
   render() {
     const { deck, onPress } = this.props;
+    const marginRight = this.state.marginRight;
 
-    const rightButtons = [
-      <TouchableOpacity onPress={() => onPress(deck.title)}>
-        <FontAwesome name="info" size={30} color="green"></FontAwesome>
-      </TouchableOpacity>,
-      <TouchableOpacity onPress={() => onPress(deck.title)}>
-        <MaterialCommunityIcons name="cards" size={30} color="green"></MaterialCommunityIcons>
-      </TouchableOpacity>
-    ]
+    const rightButtons =
+      <View>
+        <TouchableOpacity onPress={() => onPress(deck.title)}>
+          <FontAwesome name="info" size={30} color="green"></FontAwesome>
+        </TouchableOpacity>,
+        <TouchableOpacity onPress={() => onPress(deck.title)}>
+          <MaterialCommunityIcons name="cards" size={30} color="green"></MaterialCommunityIcons>
+        </TouchableOpacity>
+      </View>
 
     return (
-      <Swipeable onRef={ref => this.swipeable = ref} rightButtons={rightButtons}>
-        <TouchableOpacity style={styles.deck} onPress={() => onPress(deck.title)}>
+      <Animated.View style={{marginRight: marginRight}}>
+        <TouchableOpacity style={styles.deck} onPress={this.handleTouch}>
           <DeckHeader>{deck.title}</DeckHeader>
           <Text style={styles.text}>{`${deck.questions.length} card${deck.questions.length>1 || deck.questions.length === 0 ? "s" : ""}`}</Text>
         </TouchableOpacity>
-      </Swipeable>
+      </Animated.View>
     );
   }
 }
