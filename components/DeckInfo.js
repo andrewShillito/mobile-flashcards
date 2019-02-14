9baa2f7d48732477bae07b7144e69fca8cee74ec
andrewShillito <34523587+A-S-8@users.noreply.github.com>
2019-02-14 13:37:00 -0500

    bug: fix card/cards text depending on deck size

--- components/DeckInfo.js
+++ components/DeckInfo.js
@@ -3,10 +3,10 @@ import { View, Text } from "react-native";
 import { connect } from "react-redux";
 
 function DeckInfo(props) {
-  let numCards = props.deck ? props.deck.questions.length : "";
-  let message = numCards
-    ? numCards === 0 || numCards > 1 ? "cards" : "card"
-    : ""
+  let numCards = props.deck ? props.deck.questions.length : 0;
+  let message = numCards === 0
+    ? "cards"
+    : numCards === 1 ? "card" : "cards";
   return (
     <View style={{alignItems: "center", marginTop: 10}}>
       <Text style={{fontSize: 30}}>{`${numCards} ${message}`}</Text>
