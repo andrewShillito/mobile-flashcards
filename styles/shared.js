3ceb78928922da14557c7cf1ec91dd2155b34183
andrewShillito <34523587+A-S-8@users.noreply.github.com>
2019-02-20 12:51:15 -0500

    feat: add font constants

--- styles/shared.js
+++ styles/shared.js
@@ -3,3 +3,6 @@ export const PURPLE = "#231651";
 export const GREEN = "#4dccbd";
 export const BLUE = "#2374ab";
 export const PINK = "#ff8484"
+
+export const FONT_HEADER = "Lato";
+export const FONT_PRIMARY = "Roboto";
