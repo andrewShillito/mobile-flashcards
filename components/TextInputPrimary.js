55da0aace797cbd1a503974e52c8fe23af371c51
andrewShillito <34523587+A-S-8@users.noreply.github.com>
2019-02-21 10:13:27 -0500

    fix: make TextInputPrimary a stateless functional component

--- components/TextInputPrimary.js
+++ components/TextInputPrimary.js
@@ -3,34 +3,19 @@ import { View, TextInput } from 'react-native';
 import styles from "../styles/textInputPrimary";
 import { MAX_SHORT_INPUT_LENGTH, BLUE, BLACK } from "../styles/shared";
 
-export default class TextInputPrimary extends React.Component {
-  state = {
-    color: BLACK,
-  }
-  toggleColor = () => {
-    this.setState((prevState) => ({
-      color: prevState.color === BLACK ? BLUE : BLACK,
-    }));
-  }
-  render() {
-    const { onChangeText, value, placeholder } = this.props;
-    const color = this.state.color;
-
-    return (
-      <View style={styles.container}>
-        <TextInput
-          style={[styles.input, {borderBottomColor: color}]}
-          placeholder={placeholder}
-          onChangeText={(title) => onChangeText(title)}
-          value={value}
-          maxLength={MAX_SHORT_INPUT_LENGTH}
-          selectionColor={BLUE}
-          onFocus={() => this.toggleColor}
-          onEndEditing={() => this.toggleColor}
-          />
-      </View>
-    );
-  }
+export default function TextInputPrimary({ onChangeText, value, placeholder }){
+  return (
+    <View style={styles.container}>
+      <TextInput
+        style={styles.input}
+        placeholder={placeholder}
+        onChangeText={(title) => onChangeText(title)}
+        value={value}
+        maxLength={MAX_SHORT_INPUT_LENGTH}
+        selectionColor={BLUE}
+        />
+    </View>
+  );
 }
 
 // { onChangeText, value, placeholder }
