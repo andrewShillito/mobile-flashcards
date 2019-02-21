37930808821e9138c18400551e1f93011100a019
andrewShillito <34523587+A-S-8@users.noreply.github.com>
2019-02-21 09:57:21 -0500

    refactor: make TextInputPrimary a class

--- components/TextInputPrimary.js
+++ components/TextInputPrimary.js
@@ -1,21 +1,25 @@
 import React from 'react';
 import { View, TextInput } from 'react-native';
 import styles from "../styles/textInputPrimary";
-import { MAX_SHORT_INPUT_LENGTH, BLUE, GREEN } from "../styles/shared";
+import { MAX_SHORT_INPUT_LENGTH, BLUE } from "../styles/shared";
 
-export default function TextInputPrimary ({ onChangeText, value, placeholder }) {
-  return (
-    <View style={styles.container}>
-      <TextInput
-        style={styles.input}
-        placeholder={placeholder}
-        onChangeText={(title) => onChangeText(title)}
-        value={value}
-        maxLength={MAX_SHORT_INPUT_LENGTH}
-        selectionColor={BLUE}
-        />
-    </View>
-  );
+export default class TextInputPrimary extends React.Component {
+  render() {
+    const { onChangeText, value, placeholder } = this.props;
+
+    return (
+      <View style={styles.container}>
+        <TextInput
+          style={styles.input}
+          placeholder={placeholder}
+          onChangeText={(title) => onChangeText(title)}
+          value={value}
+          maxLength={MAX_SHORT_INPUT_LENGTH}
+          selectionColor={BLUE}
+          />
+      </View>
+    );
+  }
 }
 
 // { onChangeText, value, placeholder }
