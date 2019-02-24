080e01fa82f055fc1c760dbdaa7281979a320a80
andrewShillito <34523587+A-S-8@users.noreply.github.com>
2019-02-24 14:16:41 -0500

    chore: remove extraneous import

--- components/ButtonWarning.js
+++ components/ButtonWarning.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { View, Text, TouchableOpacity } from "react-native";
+import { Text, TouchableOpacity } from "react-native";
 import styles from "../styles/buttonWarning";
 
 function ButtonWarning({ onPress, children }) {
