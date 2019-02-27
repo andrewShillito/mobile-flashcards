716130ece358158d2692f5b1400bb0edf4aa451d
andrewShillito <34523587+A-S-8@users.noreply.github.com>
2019-02-27 13:02:16 -0500

    style: change icon colors on home screen

--- components/QuizIcon.js
+++ components/QuizIcon.js
@@ -1,13 +1,13 @@
 import React from "react";
 import { TouchableOpacity } from "react-native";
 import { MaterialCommunityIcons } from "@expo/vector-icons";
-import { INACTIVE_COLOR, GREEN, NAV_ICON_SIZE } from "../styles/shared";
+import { INACTIVE_COLOR, LIGHT_PURPLE, NAV_ICON_SIZE } from "../styles/shared";
 
 export default function QuizIcon({ onPress, active }) {
   return (
     <TouchableOpacity onPress={onPress}>
       <MaterialCommunityIcons
-        name="cards" size={NAV_ICON_SIZE} color={active ? GREEN : INACTIVE_COLOR}>
+        name="cards" size={NAV_ICON_SIZE} color={active ? LIGHT_PURPLE : INACTIVE_COLOR}>
       </MaterialCommunityIcons>
     </TouchableOpacity>
   );
