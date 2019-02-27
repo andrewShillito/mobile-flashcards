5de99256dfb0b65bbd11456206f8dd030fb52620
andrewShillito <34523587+A-S-8@users.noreply.github.com>
2019-02-27 12:25:05 -0500

    feat: complete functioning loading icon and animation

--- components/LoadingCircle.js
+++ components/LoadingCircle.js
@@ -1,108 +1,171 @@
 import React from "react";
 import { View, Animated, StyleSheet, TouchableWithoutFeedback, Text, Easing } from "react-native";
-import { WHITE, PURPLE, LIGHT_BLUE, BLUE, } from "../styles/shared";
+import { WHITE, PURPLE, LIGHT_PURPLE, LIGHT_BLUE, BLUE, } from "../styles/shared";
 
 export default class LoadingCircle extends React.Component {
   state = {
     spin1: new Animated.Value(0),
     spin2: new Animated.Value(0),
     spin3: new Animated.Value(0),
+    opac1: new Animated.Value(0),
+    opac2: new Animated.Value(0),
+    opac3: new Animated.Value(0),
+    textColor: new Animated.Value(0),
+    animation: {},
   }
   componentDidMount() {
     console.log("mounting");
-    // this.setState() //future home of starting animation and storing the return for stop func
-    Animated.loop(
-      Animated.timing(
-        this.state.spin1,
+    const animation = this.startAnimations();
+    this.setState(() => ({
+      animation: animation,
+    }));
+  }
+  startAnimations = () => {
+    const circle1Animation = Animated.loop(
+      Animated.timing(this.state.spin1,
       {
         toValue: 1,
         duration: 2000,
         easing: Easing.linear(),
-        useNativeDriver: true,
-      }),
-      {
-        iterations: 4
-      }
-    ).start();
+        useNativeDriver: false,
+      })
+    );
+    const circle2Animation = Animated.loop(
+      Animated.timing(
+      this.state.spin2, {
+        toValue: 1,
+        duration: 2000,
+        useNativeDriver: false,
+      })
+    );
+    const circle3Animation = Animated.loop(Animated.timing(
+      this.state.spin3, {
+        toValue: 1,
+        duration: 2000,
+        useNativeDriver: false,
+      })
+    );
+    Animated.parallel([
+        circle1Animation,
+        circle2Animation,
+        circle3Animation,
+        this.getCircle1OpacityAnimation(),
+        this.getCircle2OpacityAnimation(),
+        this.getCircle3OpacityAnimation(),
+        this.getTextAnimation()
+      ]).start();
   }
-  startAnimations = () => {
-    return Animated.loop(
-      Animated.parallel([
-        Animated.timing(
-          this.state.spin1,
-        {
+  getCircle1OpacityAnimation = () => {
+    return Animated.loop(Animated.sequence([
+      Animated.timing(
+        this.state.opac1, {
           toValue: 1,
           duration: 2000,
-          easing: Easing.linear(),
-          useNativeDriver: true,
+          useNativeDriver: false,
+        }),
+      Animated.timing(
+        this.state.opac1, {
+          toValue: 0,
+          duration: 2000,
+          useNativeDriver: false,
+        })
+    ]));
+  }
+  getCircle2OpacityAnimation = () => {
+    return Animated.loop(Animated.sequence([
+      Animated.timing(
+        this.state.opac2, {
+          toValue: 1,
+          duration: 1000,
+          useNativeDriver: false,
+        }),
+      Animated.timing(
+        this.state.opac2, {
+            toValue: 0,
+            duration: 1000,
+            useNativeDriver: false,
+        })
+    ]));
+  }
+  getCircle3OpacityAnimation = () => {
+    return Animated.loop(Animated.sequence([
+      Animated.timing(
+        this.state.opac3, {
+          toValue: 1,
+          duration: 1000,
+          useNativeDriver: false,
+        }),
+      Animated.timing(
+        this.state.opac3, {
+          toValue: 0,
+          duration: 1000,
+          useNativeDriver: false,
+        })
+    ]));
+  }
+  getTextAnimation = () => {
+    return Animated.loop(Animated.sequence([
+      Animated.timing(
+        this.state.textColor, {
+          toValue: 150,
+          duration: 1000,
+          useNativeDriver: false,
         }),
         Animated.timing(
-          this.state.spin2,
-          {
-            toValue: 1,
-            duration: 2000,
-            easing: Easing.liner(),
-            useNativeDriver: true,
-          }),
-        Animated.timing(
-          this.state.spin3,
-          {
-            toValue: 1,
-            duration: 2000,
-            easing: Easing.liner(),
-            useNativeDriver: true,
+          this.state.textColor, {
+            toValue: 300,
+            duration: 1000,
+            useNativeDriver: false,
           })
-      ]),
-      {
-        iterations: 4
-      }
-    ).start();
+    ]));
   }
   render() {
-    const { spin1, spin2, spin3 } = this.state;
+    const { spin1, spin2, spin3, opac1, opac2, opac3, textColor } = this.state;
 
     const interpolate1 = spin1.interpolate({
-        inputRange: [0, 1],
-        outputRange: ["0deg", "360deg"],
-      });
-    const transform1 = {
-      transform: [{ rotate: interpolate1}]
-    };
-    let interpolate2 = spin2.interpolate({
-      inputRange: [0, 1],
-      outputRange: ["0deg", "540deg"],
-    });
-    const transform2 = {
-      transform: [{ rotate: interpolate2 }]
-    }
-    let interpolate3 = spin3.interpolate({
       inputRange: [0, 1],
       outputRange: ["0deg", "720deg"],
     });
-    const transform3 = {
-      transform: [{ rotate: interpolate3 }]
+
+    const style1 = {
+      transform: [{ rotate: interpolate1}],
+      opacity: opac1,
+    };
+    const interpolate2 = spin2.interpolate({
+      inputRange: [0, 0.25, 0.5, 0.75, 1],
+      outputRange: ["0deg", "0deg", "270deg", "405deg", "720deg"],
+    });
+    const style2 = {
+      transform: [{ rotate: interpolate2 }],
+      opacity: opac2,
     }
+    const interpolate3 = spin3.interpolate({
+      inputRange: [0, 1],
+      outputRange: ["0deg", "1080deg"],
+    });
+    const style3 = {
+      transform: [{ rotate: interpolate3 }],
+      opacity: opac3,
+    }
+    const interpolateColor = textColor.interpolate({
+      inputRange: [0, 150, 300],
+      outputRange: [PURPLE, LIGHT_PURPLE, PURPLE],
+    })
+    const textStyle = {
+      color: interpolateColor,
+    };
     return (
       <View style={styles.container}>
-        <Animated.View style={[styles.circle1, transform1]}>
-          <Animated.View style={[styles.circle2, transform2]}>
-            <Animated.View style={[styles.circle3, transform3]}>
-                <Text style={styles.text}>Loading...</Text>
-            </Animated.View>
-          </Animated.View>
-        </Animated.View>
+        <View style={styles.container}>
+          <Animated.View style={[styles.circle1, style1]}></Animated.View>
+          <Animated.View style={[styles.circle2, style2]}></Animated.View>
+          <Animated.View style={[styles.circle3, style3]}></Animated.View>
+          <Animated.Text style={[styles.text, textStyle]}>Loading</Animated.Text>
+        </View>
       </View>
     );
   }
 }
-
-// Additional circles:
-
-//   <Animated.View style={[styles.circle2, {transform: [{rotate: interpolate2}]}]}>
-//     <Animated.View style={[styles.circle3, {transform: [{rotate: interpolate3}]}]}>
-//       <Text style={styles.text}>Loading</Text>
-//     </Animated.View>
-//   </Animated.View>
 
 const dimen1 = 192;
 const dimen2 = 160;
@@ -111,13 +174,21 @@ const dimen3 = 128;
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    backgroundColor: LIGHT_BLUE,
     alignItems: "center",
     justifyContent: "center",
+  },
+  loaderContainer: {
+    position: "relative",
     backgroundColor: LIGHT_BLUE,
+    margin: "auto",
+    height: dimen1,
+    width: dimen1,
   },
   circle1: {
     height: dimen1,
     width: dimen1,
+    position: "absolute",
     borderStyle: "solid",
     borderWidth: 7,
     borderRadius: dimen1/2,
@@ -125,12 +196,11 @@ const styles = StyleSheet.create({
     borderRightColor: LIGHT_BLUE,
     borderBottomColor: BLUE,
     borderLeftColor: LIGHT_BLUE,
-    justifyContent: "center",
-    alignItems: "center",
   },
   circle2: {
     height: dimen2,
     width: dimen2,
+    position: "absolute",
     borderStyle: "solid",
     borderWidth: 6,
     borderRadius: dimen2/2,
@@ -138,12 +208,11 @@ const styles = StyleSheet.create({
     borderRightColor: LIGHT_BLUE,
     borderBottomColor: LIGHT_BLUE,
     borderLeftColor: BLUE,
-    justifyContent: "center",
-    alignItems: "center",
   },
   circle3: {
     height: dimen3,
     width: dimen3,
+    position: "absolute",
     borderStyle: "solid",
     borderWidth: 5,
     borderRadius: dimen3/2,
@@ -151,11 +220,9 @@ const styles = StyleSheet.create({
     borderRightColor: BLUE,
     borderBottomColor: LIGHT_BLUE,
     borderLeftColor: LIGHT_BLUE,
-    justifyContent: "center",
-    alignItems: "center",
   },
   text: {
-    color: PURPLE,
+    color: WHITE,
     fontSize: 25,
   },
 });
--- styles/shared.js
+++ styles/shared.js
@@ -1,5 +1,6 @@
 export const LIGHT_BLUE = "#d6fff6";
 export const PURPLE = "#231651";
+export const LIGHT_PURPLE = "#6750ba";
 export const GREEN = "#4dccbd";
 export const BLUE = "#2374ab";
 export const PINK = "#ff8484";
