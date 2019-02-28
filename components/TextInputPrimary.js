f022b78fc77d3133796147a6f61876530d3fa8b2
andrewShillito <34523587+A-S-8@users.noreply.github.com>
2019-02-27 21:41:55 -0500

    bug: it seems somehow impossible to change the borderColor of TextInput on focus?

--- components/TextInputPrimary.js
+++ components/TextInputPrimary.js
@@ -1,21 +1,33 @@
 import React from 'react';
 import { View, TextInput } from 'react-native';
 import styles from "../styles/textInputPrimary";
-import { MAX_SHORT_INPUT_LENGTH, BLUE, BLACK } from "../styles/shared";
+import { MAX_SHORT_INPUT_LENGTH, BLUE, BLACK, PINK } from "../styles/shared";
 
-export default function TextInputPrimary({ onChangeText, value, placeholder }){
-  return (
-    <View style={styles.container}>
-      <TextInput
-        style={styles.input}
-        placeholder={placeholder}
-        onChangeText={(title) => onChangeText(title)}
-        value={value}
-        maxLength={MAX_SHORT_INPUT_LENGTH}
-        selectionColor={BLUE}
-        />
-    </View>
-  );
+export default class TextInputPrimary extends React.Component {
+  state = {
+    borderColor: BLACK,
+  }
+  toggleColor = () => {
+    // this._textInput.setNativeProps({style: {borderBottomColor: PINK}})
+    // console.log((this._textInput.setNativeProps).toSource());
+    // console.log(this._textInput._inputRef.viewConfig.validAttributes.style.borderBottomColor.process(PINK));
+  }
+  render() {
+    const { onChangeText, value, placeholder } = this.props;
+    const { borderColor } = this.state;
+    return (
+      <View style={styles.container}>
+        <TextInput
+          ref={(component) => this._textInput = component}
+          style={styles.input}
+          placeholder={placeholder}
+          onChangeText={(title) => onChangeText(title)}
+          value={value}
+          maxLength={MAX_SHORT_INPUT_LENGTH}
+          selectionColor={BLUE}
+          onFocus={this.toggleColor}
+          />
+      </View>
+    );
+  }
 }
-
-// { onChangeText, value, placeholder }
