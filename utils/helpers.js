2dc55a605358b3cc611db76337e23a8457d1a9cf
andrewShillito <34523587+A-S-8@users.noreply.github.com>
2019-03-01 10:32:29 -0500

    feat: build logging code

--- utils/helpers.js
+++ utils/helpers.js
@@ -9,3 +9,14 @@ export function validateUniqueDeckName(text, deckNames) {
 export function validateUniqueCard(question, answer, questions){
   return questions.some((card) => card.question === question && card.answer === answer);
 }
+
+export const getName = (fn) => { // get function name for error logging
+  // source: http://jsfiddle.net/ncays/ and
+  // https://stackoverflow.com/questions/1013239/can-i-get-the-name-of-the-currently-running-function-in-javascript
+  var match = fn.arguments.callee.toString().match(/function\s+([^\s\(]+)/);
+  return match && match.length ? match[1] : match;
+}
+
+export const logResponse = (func, res) => {
+  return console.log(getName(func), JSON.stringify(res));
+}
