91f88d1aaa65455d08f0b948f60c7a6aea70793d
andrewShillito <34523587+A-S-8@users.noreply.github.com>
2019-03-02 08:39:16 -0500

    feat: build card table & scores table query constants

--- utils/queries.js
+++ utils/queries.js
@@ -1,10 +1,11 @@
-export const createDecks = "create table if not exists decks" +
+export const createDecks = "create table if not exists decks " +
 ([
   "(title TEXT PRIMARY KEY NOT NULL",
   "category TEXT",
   "create_date TEXT NOT NULL",
   "last_tested TEXT",
-  "last_accessed TEXT NOT NULL)"
+  "last_accessed TEXT NOT NULL",
+  "last_score REAL)"
 ].join(", "))+;
 
 export const getDecks = "SELECT * FROM decks";
@@ -21,7 +22,47 @@ export const updateDeckTitle = "UPDATE decks SET title=? WHERE title=?";
 
 export const renameTable = "ALTER TABLE ? RENAME TO ?";
 
-export const createCardTable = "CREATE TABLE IF NOT EXISTS ?" +
+// if each card is stored in a table with all other cards and foreign key === deck name
+export const createCards = "CREATE TABLE IF NOT EXISTS cards " + ([
+  "(card_id INTEGER NOT NULL PRIMARY KEY AUTO_INCREMENT",
+  "deck_id TEXT NOT NULL",
+  "question TEXT NOT NULL",
+  "answer TEXT NOT NULL",
+  "FOREIGN KEY (deck_id) REFERENCES decks(title))"
+]).join(", ");
+
+export const createCard = "INSERT INTO cards (deck_id, question, answer) VALUES (?, ?, ?)";
+
+export const getAllCards = "SELECT * FROM cards";
+
+export const getCardsFromDeck = "SELECT * FROM cards WHERE deck_id=?";
+
+export const removeCard = "DELETE FROM cards WHERE question=?, answer=?";
+// in future may just use index (card_id) which could be easier query - refactor later
+
+export const removeAllCardsFromDeck = "DELETE FROM cards WHERE deck_id=?";
+
+export const updateCard = "UPDATE cards SET question=?, answer=? WHERE deck_id=?, question=?, answer=?";
+// lots of params, think about how to shorten the necessary params for func call
+
+export const createScores = "CREATE TABLE IF NOT EXISTS scores " + ([
+  "(score_id INT NOT NULL PRIMARY KEY AUTO_INCREMENT",
+  "deck_id TEXT NOT NULL",
+  "time TEXT NOT NULL",
+  "score REAL NOT NULL",
+  "FOREIGN KEY (deck_id) REFERENCES decks(title))"
+]).join(", ");
+
+export const recordScore = "INSERT INTO scores (deck_id, time, score) VALUES (?, ?, ?)";
+
+export const getAllScores = "SELECT * FROM scores";
+
+export const getAllScoresFromDeck = "SELECT * FROM scores WHERE deck_id=?";
+
+export const removeAllScoresFromDeck = "DELETE FROM scores WHERE deck_id=?";
+
+// deprecated - if each card table is named after the deck it is from
+export const createCardTable = "CREATE TABLE IF NOT EXISTS ? " +
 ([
   "(card_id INTEGER PRIMARY KEY NOT NULL auto_increment",
   "question TEXT NOT NULL",
